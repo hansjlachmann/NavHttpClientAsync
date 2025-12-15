86667744fd8dac81048c418bc04422cfa5a99225
Manuel Rojas <leuamrojas@hotmail.com>
2025-12-15 15:07:31 +0100

    Add project files.

---
+++ NavHttpClient.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.InteropServices;
+
+namespace NavHttpClientAsync
+{
+    [Guid("4c301f44-63e0-11ee-8c99-0242ac120002")]
+    [ComVisible(true)]
+    public interface INavHttpClient
+    {
+       string HelloWorld();
+    }
+
+    [Guid("3543cf38-63e0-11ee-8c99-0242ac120002")]
+    [ClassInterface(ClassInterfaceType.None)]
+    [ComVisible(true)]
+    public class NavHttpClient : INavHttpClient
+    {
+        public string HelloWorld()
+        {
+            return "Hello World!";
+        }
+    }
+}
